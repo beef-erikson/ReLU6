605c381cad15a12787b3ff7953da6e7eb1659763
Troy Martin <beef.erikson.studios@gmail.com>
2022-10-15 13:18:03 -0400

    Modified parameters and added comments

--- mlp-with-relu.py
+++ mlp-with-relu.py
@@ -6,15 +6,16 @@ from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.initializers import RandomUniform
 
-X, y = make_circles(n_samples=1000, noise=0.1, random_state=1)
-# scale input data to [-1,1]
+# Generate dataset
+X, y = make_circles(n_samples=600, noise=0.1, random_state=1)
 scaler = MinMaxScaler(feature_range=(-1, 1))
 X = scaler.fit_transform(X)
 
-n_train = 500
+n_train = 1000
 trainX, testX = X[:n_train, :], X[n_train:, :]
 trainy, testy = y[:n_train], y[n_train:]
 
+# Define model
 init = RandomUniform(minval=0, maxval=1)
 model = Sequential()
 model.add(Dense(5, input_dim=2, activation='relu',
@@ -34,6 +35,7 @@ model.add(Dense(1, activation='sigmoid'))
 opt = SGD(learning_rate=0.01, momentum=0.9)
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
+# Configure and plot model
 history = model.fit(trainX, trainy, validation_data=(
     testX, testy), epochs=500, verbose=0)
 
